28cfee5a441e4310f011c85b98823b5a3376ac66
FRUHD <99921514+FRUHD@users.noreply.github.com>
2022-04-28 21:50:00 -0700

    Whew, basic framework and code done. Now for GUI...

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
 
 class Board {
     constructor() {
-        //store winningConditions as an array of positions, where 0 is top left, 8 is bottom right
+        //Store winningConditions as an array of positions, where 0 is top left, 8 is bottom right
         this.winningConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,7],[2,4,8]];
         //Board stores player one's moves (AKA player 'X')
         this.playerOneMoves = []; 
@@ -29,36 +29,49 @@ class Board {
         this.playerTwoMoves = []; 
         //Board stores all used moves
         this.usedMoves = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        //Game always starts with 'Player X'
         this.turnX = true
+        //When someone wins OR there are no more empty spaces
+        this.gameOver = false
     }
   
     // Prompt each player's turn:
     // 1. Determine if location is available && if so, update usedMoves
     // 2. Give location to respective player (array)
     // 3. Check if player has won
-    // 4. If no win, change player's turn
+    // 4. Check if there are any more moves to play (if all values are 1)
+    // 5. If no win, change player's turn
     playerTurn(location) {
       // 1. Determine if location is available && if so, update usedMoves
-      if (this.usedMoves[location] === !0) {
+      if (this.usedMoves[location] === 1) {
         //Player cannot go here
+        console.log('This square has already been chosen. Please choose again.')
         return
       } else {
         this.usedMoves[location] = 1
       }
       // 2. Give location to respective player (array)
-      turnX ? this.playerOneMoves.push(location) : this.playerTwoMoves.push(location)
+      this.turnX ? this.playerOneMoves.push(location) : this.playerTwoMoves.push(location);
+      let playerName = this.turnX ? "Player X" : "Player O";
+      console.log(`${playerName} has selected ${location}`)
       // 3. Check if player has won
-      // 4. If no win, change player's turn
+      const arrayToUse = this.turnX ? this.playerOneMoves : this.playerTwoMoves
+      if (this.determineWin(arrayToUse)) {
+        // Winner winner !checkin dinner!
+        document.querySelector('h3').innerText = `${playerName} has won!`
+        this.gameOver = true
+        return
+      }
+      // 4. Check if there are any more moves to play (if all values are 1)
+      if (this.usedMoves.includes(0)) {
+        // 5. If no win, change player's turn
+        this.turnX = !this.turnX  // Courtesy of Dahne!
+      } else {
+        document.querySelector('h3').innerText = `It's a draw! (There are no more turns left)`
+        this.gameOver = true
+        return
+      }
     }
-  
-
-  //Just thinking down here - would this work?
-  const changeTurn = ()=>{
-    turnX = !turnX;
-  }
-  
-  //just call changeTurn() at the end of each turn, i.e. when a square/cell is clicked
-
 
     // Check if player has won
     determineWin(moves) {
@@ -78,7 +91,7 @@ class Board {
 }
   
   const ourBoard = new Board()
-  ourBoard.testDetermineWin()
+  // ourBoard.testDetermineWin()
     
     
    /* Notesssssss
@@ -95,7 +108,6 @@ class Board {
         break;
       case 
   }
-  
     
   let board = [['','',''],['','',''],['','','']]
   
@@ -116,16 +128,9 @@ class Board {
       i; ;  )
   }
   
-  
-
-  
-  
-  
-  
   let gamesMoves = ['x','x','x','0']
     
   //check both Arrays to see if they contain any of those winning conditions;
-  
   //write function to check the winningConditions against what positions the player has in their own array
   
   */
